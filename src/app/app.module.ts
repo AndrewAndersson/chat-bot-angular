773c365eec9c06e4b589868c339869312214cd76
AndrewAndersson <anderssona258@gmail.com>
2018-07-24 23:49:50 +0300

    create new app

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AngularBotModule } from './angular-bot/angular-bot.module';
+import { ChatService } from './angular-bot/chat.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularBotModule
+  ],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
